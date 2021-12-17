b814f9762d7b4503080eb0f58342449b5225ad6e
Matthijs4004 <matthijs4004@gmail.com>
2021-12-17 09:17:15 +0100

    lightswitch in js&html is working

--- lightswitch/script.js
+++ lightswitch/script.js
@@ -1,10 +1,21 @@
 var btn = document.createElement('button');
 btn.style.margin = '10px';
-btn.innerHTML = '...';
+btn.innerHTML = 'lightswitch off';
+document.getElementById(button_id).innerHTML = 'lightswitch off';
 document.body.appendChild(btn);
 
-// schijf hier tussen je code
-
-
 
 // schijf hier tussen je code
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("lighton");
+    var x = document.getElementById("switch");
+  if (x.innerHTML === "lightswitch off") {
+    x.innerHTML = "lightswitch on";
+    console.log("light is on!");
+  } else {
+    x.innerHTML = "lightswitch off";
+    console.log("light is off!");
+  }
+}
+    // schijf hier tussen je code
